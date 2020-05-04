2b22ad0c9a09c81e1ea0802dd52489778de64d0c
Joel <jfb.sene@gmail.com>
2020-05-04 14:46:17 -0300

    Initial Commit
    The initial commit contains the newly created project and the essential pods:
    - Alamofire for networking
    - IQKeyboardManager to deal with keyboard
    - PKHud to handle ProgressHUD
    - SwiftyJson to handle Json ( might not be necessary)

---
+++ MoBApp/MoBApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoBApp
+//
+//  Created by Joel Sene on 04/05/20.
+//  Copyright © 2020 Joel Sene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
